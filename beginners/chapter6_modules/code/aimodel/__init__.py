b4b5c01b2d26b3c827c0a1377aed8ec44d63bb46
Vijay Bheemineni <vijaybheemineni@github.com>
2026-02-03 09:42:49 -0600

    Create basic structure for my 'aimodel' package.

---
+++ beginners/chapter6_modules/code/aimodel/__init__.py
@@ -0,0 +1,8 @@
+"""
+AI Model Package - Hello World Example
+A package for building, training, and deploying AI models for my understanding
+"""
+print("Hello from vijay aimodel package!")
+
+__version__ = "0.1.0"
+__author__ = "Vijay Bheemineni"
---
+++ beginners/chapter6_modules/code/aimodel/training/__init__.py
@@ -0,0 +1,1 @@
+"""Subpackage for training utilities"""
